1ad7b371796a7b9f242be011143fd7b116fbd7a4
Blundell89 <leeblundell16@gmail.com>
2016-02-14 20:15:54 +0000

    Added integration tests proj

---
+++ src/LiberisLabs.MassTransit.Persistence.MongoDb.IntegrationTests/LocatingAnExistingSagaTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MassTransit;
+using NUnit.Framework;
+
+namespace LiberisLabs.MassTransit.Persistence.MongoDb.IntegrationTests
+{
+    [TestFixture]
+    public class LocatingAnExistingSagaTests
+    {
+        private Guid _sagaId;
+
+        [OneTimeSetUp]
+        public void GivenASimpleSaga()
+        {
+            _sagaId = Guid.NewGuid();
+            var message = new SimpleSagaMessage(_sagaId);
+
+
+        }
+    }
+
+    public class SimpleSagaMessage : CorrelatedBy<Guid>
+    {
+        public SimpleSagaMessage(Guid correlationId)
+        {
+            CorrelationId = correlationId;
+        }
+
+        public Guid CorrelationId { get; }
+    }
+}
