9a389854c5a576cbaa0fc54dc8a2f754fd50f80f
Lee Blundell <leeblundell16@gmail.com>
2016-03-01 09:40:30 +0000

    Updated test

---
+++ src/LiberisLabs.MassTransit.Persistence.MongoDb.Tests/Saga/MongoDbSagaConsumeContextTestsForPopContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MassTransit;
+using MassTransit.Persistence.MongoDb.Saga;
+using MongoDB.Driver;
+using Moq;
+using NUnit.Framework;
+
+namespace LiberisLabs.MassTransit.Persistence.MongoDb.Tests.Saga
+{
+    [TestFixture]
+    public class MongoDbSagaConsumeContextTestsForPopContext
+    {
+        private SagaConsumeContext<SimpleSaga, InitiateSimpleSaga> _context;
+
+        [OneTimeSetUp]
+        public void GivenAMongoDbSagaConsumeContext_WhenPoppingContext()
+        {
+            var mongoDbSagaConsumeContext = new MongoDbSagaConsumeContext<SimpleSaga, InitiateSimpleSaga>(Mock.Of<IMongoCollection<SimpleSaga>>(), Mock.Of<ConsumeContext<InitiateSimpleSaga>>(), Mock.Of<SimpleSaga>());
+
+            _context = mongoDbSagaConsumeContext.PopContext<InitiateSimpleSaga>();
+        }
+
+        [Test]
+        public void ThenMongoContextReturnedAsSagaConsumeContext()
+        {
+            Assert.That(_context, Is.Not.Null);
+        }
+    }
+}
